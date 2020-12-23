9edbd3379d26aed19f3b181453dc6aaa2dbc7b8c
RomanSeveryn <romaseverin50@gmail.com>
2020-12-23 15:26:55 +0200

    Add function isTriangle

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const isAdult = function (age) {
 }
 
 
-const checkMultiplicity = function () {
-  const firstNumber = +prompt('Введите первое число');
-  const secondNumber = +prompt('Введите второе число');
+const checkMultiplicity = function (firstNumber, secondNumber) {
+  // const firstNumber = +prompt('Введите первое число');
+  // const secondNumber = +prompt('Введите второе число');
 
 
   if (firstNumber % secondNumber === 0) {
@@ -18,4 +18,15 @@ const checkMultiplicity = function () {
   return false;
 }
 
-console.log(checkMultiplicity());
+console.log(checkMultiplicity())
+
+const isTriangle = function(a, b, c) {
+  // const firstSide = +prompt('Введите первую сторону');
+  // const secondSide = +prompt('Введите второую сторону');
+  // const thirdSide = +prompt('Введите третью сторону');
+
+  if(a > 0 && b > 0 && c > 0) {
+    return true;
+  }
+  return false;
+}
