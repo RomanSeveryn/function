37dc1e459f20dcf1976018af20f01f02b695c01f
RomanSeveryn <romaseverin50@gmail.com>
2020-12-23 15:12:56 +0200

    Add function checkMultiplicity

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 
-const isAdult = function(age) {
-  if(age >= 18) {
+const isAdult = function (age) {
+  if (age >= 18) {
     return true;
   }
   return false;
 }
 
 
-console.log(isAdult());
+const checkMultiplicity = function () {
+  const firstNumber = +prompt('Введите первое число');
+  const secondNumber = +prompt('Введите второе число');
+
+
+  if (firstNumber % secondNumber === 0) {
+    return true;
+  }
+  return false;
+}
+
+console.log(checkMultiplicity());
