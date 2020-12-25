4a76c2c31b96bc38a2436fc32f1525c5695ce4c6
RomanSeveryn <romaseverin50@gmail.com>
2020-12-25 20:15:21 +0200

    Add functions area

--- index.js
+++ index.js
@@ -1,32 +1,74 @@
-
 const isAdult = function (age) {
-  if (age >= 18) {
-    return true;
+  if (isNaN(age)) {
+    console.log('Невалидное число')
+  } else {
+    if (age >= 18) {
+      return true;
+    }
   }
   return false;
 }
 
 
+
+
 const checkMultiplicity = function (firstNumber, secondNumber) {
-  // const firstNumber = +prompt('Введите первое число');
-  // const secondNumber = +prompt('Введите второе число');
-
-
   if (firstNumber % secondNumber === 0) {
     return true;
   }
   return false;
 }
 
-console.log(checkMultiplicity())
 
-const isTriangle = function(a, b, c) {
-  // const firstSide = +prompt('Введите первую сторону');
-  // const secondSide = +prompt('Введите второую сторону');
-  // const thirdSide = +prompt('Введите третью сторону');
 
-  if(a > 0 && b > 0 && c > 0) {
+
+const isTriangle = function (a, b, c) {
+  if (a > 0 && b > 0 && c > 0) {
     return true;
   }
   return false;
+}
+
+
+const areaRhombus = function(diagonalVertical, diagonalHorizontal) {
+  if(isNaN(diagonalVertical - diagonalHorizontal)) {
+    return false;
+  } else {
+    return diagonalVertical * diagonalHorizontal / 2;
+  }
+}
+
+
+
+const areaCylinder = function(heightCylinder, radiusCylinder) {
+  // S = 2 * 3.14 * r(h + r);
+  if(isNaN(heightCylinder - radiusCylinder)) {
+    return false;
+  } else {
+    return (2 * 3.14 * radiusCylinder) *(heightCylinder + radiusCylinder);
+  }
+}
+
+
+const areaTriangle = function(baseSide, heightTriangle) {
+  if(isNaN(baseSide - heightTriangle)) {
+    return false;
+  } else  {
+    return 1/2 * baseSide * heightTriangle;
+  }
+}
+
+
+
+
+
+
+
+
+const areaRectangle = function(sideLateral, sideBase) {
+  if(isNaN(sideLateral - sideBase)){
+    return false;
+  } else {
+    return sideLateral * sideBase;
+  }
 }
