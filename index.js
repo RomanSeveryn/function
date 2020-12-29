3c15140462ec4a4979082b44325b80ed00c9ffcd
RomanSeveryn <romaseverin50@gmail.com>
2020-12-29 10:32:58 +0200

    Add fixed cod

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const isAdult = function (age) {
   if (isNaN(age)) {
-    console.log('Невалидное число')
-  } else {
-    if (age >= 18) {
-      return true;
-    }
+    return null;
   }
-  return false;
+  return age >= 18;
 }
 
 
 
 
 const checkMultiplicity = function (firstNumber, secondNumber) {
-  if (firstNumber % secondNumber === 0) {
-    return true;
-  }
-  return false;
+  return (firstNumber % secondNumber === 0)
 }
 
 
@@ -30,43 +23,35 @@ const isTriangle = function (a, b, c) {
 }
 
 
-const areaRhombus = function(diagonalVertical, diagonalHorizontal) {
-  if(isNaN(diagonalVertical - diagonalHorizontal)) {
+const areaRhombus = function (diagonalVertical, diagonalHorizontal) {
+  if (isNaN(diagonalVertical - diagonalHorizontal)) {
     return false;
-  } else {
-    return diagonalVertical * diagonalHorizontal / 2;
   }
+  return diagonalVertical * diagonalHorizontal / 2;
 }
 
 
 
-const areaCylinder = function(heightCylinder, radiusCylinder) {
+const areaCylinder = function (heightCylinder, radiusCylinder) {
   // S = 2 * 3.14 * r(h + r);
-  if(isNaN(heightCylinder - radiusCylinder)) {
+  if (isNaN(heightCylinder - radiusCylinder)) {
     return false;
-  } else {
-    return (2 * 3.14 * radiusCylinder) *(heightCylinder + radiusCylinder);
   }
+  return (2 * 3.14 * radiusCylinder) * (heightCylinder + radiusCylinder);
 }
 
 
-const areaTriangle = function(baseSide, heightTriangle) {
-  if(isNaN(baseSide - heightTriangle)) {
+const areaTriangle = function (baseSide, heightTriangle) {
+  if (isNaN(baseSide - heightTriangle)) {
     return false;
-  } else  {
-    return 1/2 * baseSide * heightTriangle;
   }
+  return 1 / 2 * baseSide * heightTriangle;
 }
 
 
 
-
-
-
-
-
-const areaRectangle = function(sideLateral, sideBase) {
-  if(isNaN(sideLateral - sideBase)){
+const areaRectangle = function (sideLateral, sideBase) {
+  if (isNaN(sideLateral - sideBase)) {
     return false;
   } else {
     return sideLateral * sideBase;
